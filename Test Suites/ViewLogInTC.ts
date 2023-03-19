a6c655853c6dc016efad4d291ed6fd4ac929877b
ALBADR FLOUS <ALBADR FLOUS@192.168.20.72>
2023-03-19 11:34:45 +0200

    polo

---
+++ Test Suites/ViewLogInTC.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ViewLogInTC</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>64c0a222-70a8-49d9-abab-5df7120ac368</testSuiteGuid>
+   <testCaseLink>
+      <guid>ceaf4005-c61c-4674-a71a-69ec2ce997a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ViewLogInTc</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
